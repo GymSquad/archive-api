b97e7d12643f71da53fb90c63638e91edfe8e966
BaBa0525 <hsunhsun0525@gmail.com>
2023-08-18 01:29:21 +0800

    ✨ add example api

--- main.go
+++ main.go
@@ -3,13 +3,24 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"path/filepath"
 
+	"github.com/gin-gonic/gin"
 	_ "github.com/joho/godotenv/autoload"
 )
 
 func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run()
+
 	rootPath := os.Getenv("ROOT_PATH")
 	fmt.Println(rootPath)
 
