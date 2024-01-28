bc3af15ef39df998b340f7fa65336defd8072989
BaBa0525 <103883300+BaBa0525@users.noreply.github.com>
2024-01-28 15:21:27 +0800

    ci: check openapi (test1)

--- app/openapi.py
+++ app/openapi.py
@@ -1,11 +1,13 @@
 import json
-from pathlib import Path
+
+import click
 
 from app.app import app
 
 
-def main():
-    dump_path = Path(__file__).parent.parent / "openapi.json"
+@click.command(name="dump-schema", help="Dump OpenAPI schema to a file")
+@click.option("--dump-path", "-D", type=click.Path(), default="openapi.json", help="Path to dump OpenAPI schema")
+def dump_schema(dump_path: str):
     schema = app.openapi()
 
     with open(dump_path, "w") as schema_file:
@@ -13,4 +15,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    dump_schema()
