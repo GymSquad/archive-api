1ef5fcea85fd595a3a3652a6c42fdeace67639cd
BaBa0525 <103883300+BaBa0525@users.noreply.github.com>
2024-02-19 20:26:23 +0800

    chore: update category id response

--- app/category/routes.py
+++ app/category/routes.py
@@ -1,6 +1,7 @@
 from typing import Annotated
 
 from fastapi import APIRouter, Depends
+from pydantic import BaseModel, ConfigDict
 
 from app import db
 from app.db import Record
@@ -8,10 +9,26 @@ from app.db import Record
 router = APIRouter(prefix="/api/category", tags=["category"])
 
 
+class AllCategoriesIdResponse(BaseModel):
+    ids: list[str]
+
+    model_config = ConfigDict(
+        json_schema_extra={
+            "example": {
+                "ids": [
+                    "clhjj1v7c0000gbgn8a5z182n",
+                    "clhjj1v9k0002gbgnbfju6luj",
+                    "clhjj1v9m0004gbgn0gsst205",
+                ]
+            }
+        }
+    )
+
+
 @router.get("/id")
 async def get_all_categories_id(
     db: Annotated[db.Connection, Depends(db.get_db)],
-) -> list[str]:
+) -> AllCategoriesIdResponse:
     campuses: list[Record] = await db.fetch('SELECT id FROM "Category";')
 
-    return [campus["id"] for campus in campuses]
+    return AllCategoriesIdResponse(ids=[campus["id"] for campus in campuses])
